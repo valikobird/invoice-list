cd4299a424a60468806c4fd1089eba23bd6aebc3
Valentyn Zozulia <valikobird@gmail.com>
2021-03-13 15:42:42 +0000

    Improve table header test

---
+++ src/components/TableHeader/TableHeader.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import TableHeader from ".";
+import { columns } from '../../config.json';
+
+describe('TableHeader', () => {
+    it('has all defined columns', () => {
+        render(<table><TableHeader columns={columns} /></table>);
+
+        const values = Object.values(columns);
+        expect(values.length).toBeGreaterThan(0);
+
+        values.forEach(value => {
+            expect(screen.getByText(value)).toBeInTheDocument();
+        });
+    });
+});
