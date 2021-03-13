e0eb080841d4fed42abb5be2ff00d92f6e2b5f79
Valentyn Zozulia <valikobird@gmail.com>
2021-03-13 15:48:49 +0000

    Improve table test

--- src/components/Table/Table.test.js
+++ src/components/Table/Table.test.js
@@ -1,6 +1,10 @@
 import { render, screen } from '@testing-library/react';
-import { checkTableHeaders } from '../testUtils';
+import ReactSeventeenAdapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { configure, mount } from 'enzyme';
+import TableHeader from '../TableHeader';
 import Table from './index';
+
+configure({ adapter: new ReactSeventeenAdapter() });
 
 const data = [{
     "status": "Final",
@@ -11,16 +15,20 @@ const data = [{
     "total_w_vat": 123.42
 }];
 
-test('table headers inside invoices table', () => {
-    checkTableHeaders(<Table invoices={data} />);
-});
+describe('Table', () => {
+    it('should contain column headers', () => {
+        const wrapper = mount(<Table invoices={data} />);
+        expect(wrapper.props().invoices).toBeDefined();
+        expect(wrapper.find(TableHeader).length).toBe(1);
+    });
 
-test('table row data inside invoices table', () => {
-    render(<Table invoices={data} />);
+    it('should have row data', () => {
+        render(<Table invoices={data} />);
 
-    data.forEach(item => {
-        Object.values(item).forEach(value => {
-            expect(screen.getByText(value)).toBeInTheDocument();
-        });    
+        data.forEach(item => {
+            Object.values(item).forEach(value => {
+                expect(screen.getByText(value)).toBeInTheDocument();
+            });
+        });
     });
 });
